057cc9b6da083dfd6e2261901250b2f3362640ba
Tommy <tommy_8786@hotmail.com>
2020-09-17 10:06:43 +0200

    Test commit

--- Lab2/Lab2/Shape3D.cs
+++ Lab2/Lab2/Shape3D.cs
@@ -6,7 +6,7 @@ using System.Text;
 namespace Lab2
 {
     class Shape3D : Shape
-    {
+    { //test
         public override Vector3 Center => throw new NotImplementedException();
 
         public override float Area => throw new NotImplementedException();
