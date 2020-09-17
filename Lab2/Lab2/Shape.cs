b9fae1625fffe5cabfbb7a08d87fa6f53d72c278
Kristian Golba <kristian.golba@iths.se>
2020-09-17 09:57:29 +0200

    Added 2D Shape.

--- Lab2/Lab2/Shape.cs
+++ Lab2/Lab2/Shape.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Numerics;
 
 namespace Lab2
 {
     public abstract class Shape
     {
+        public abstract Vector3 Center { get; }
+        public abstract float Area { get; }
     }
 }
---
+++ Lab2/Lab2/Shape2D.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Lab2
+{
+    class Shape2D : Shape
+    {
+        public override Vector3 Center => throw new NotImplementedException();
+
+        public override float Area => throw new NotImplementedException();
+    }
+}
